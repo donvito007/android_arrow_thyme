fb5646dae05214f0217df88bffd29063394ea6e2
easterNday <849919718@qq.com>
2023-03-15 23:00:43 +0800

    gapps: build with GAPPS & set version to beta
    Change-Id: I8d98500236f28688fd3fdab624097fd051cdb3f0

--- arrow_thyme.mk
+++ arrow_thyme.mk
@@ -26,3 +26,13 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/thyme/thyme:12/SKQ1.211006.001/V13.0.8.0.SGACNXM:user/release-keys
+
+# Build with gapps
+# ARROW_BETA := true
+ARROW_GAPPS := true
+
+# 解决 WIFI 和时区问题
+NTP_SERVER := asia.pool.ntp.org
+PRODUCT_PROPERTY_OVERRIDES += \
+    captive_portal_http_url=http://connect.rom.miui.com/generate_204 \
+    captive_portal_https_url=https://connect.rom.miui.com/generate_204
